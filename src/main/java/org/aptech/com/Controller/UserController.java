8fb06a890052f47fcf62dcbae892c258c0d4ce76
AleeyahAnjola <anjolaoluwami20@gmail.com>
2022-08-04 11:35:34 +0100

    Second Commit

--- src/main/java/org/aptech/com/Controller/UserController.java
+++ src/main/java/org/aptech/com/Controller/UserController.java
@@ -1,5 +1,18 @@
 package org.aptech.com.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
 
+	@GetMapping("/home")
+	public String home() {
+		return "Welcome to Home Page :)";
+	}
+	
+	@GetMapping("/dashboard")
+	public String dashboard() {
+		return "Welcome to Dashboard Page :)";
+	}
 }
