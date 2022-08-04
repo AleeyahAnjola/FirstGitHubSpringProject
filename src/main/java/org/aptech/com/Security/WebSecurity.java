46d82bccb986478b3a6062a6fe8fbb9791b15313
AleeyahAnjola <anjolaoluwami20@gmail.com>
2022-08-04 11:50:24 +0100

    Third Commit -- Http Security Configuration

---
+++ src/main/java/org/aptech/com/Security/WebSecurity.java
@@ -0,0 +1,13 @@
+package org.aptech.com.Security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests().antMatchers("/dashboard").authenticated()
+		.antMatchers("/home").permitAll().and().httpBasic();		
+	}
+}
